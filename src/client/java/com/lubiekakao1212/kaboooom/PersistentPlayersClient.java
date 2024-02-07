d6fe1f4f6f9b56036eac195a90086f25bb2954bc
LubieKakao1212 <grzesio.go@gmail.com>
2024-02-07 11:25:05 +0100

    Initial Commit + Project Setup

---
+++ src/client/java/com/lubiekakao1212/kaboooom/PersistentPlayersClient.java
@@ -0,0 +1,10 @@
+package com.lubiekakao1212.kaboooom;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PersistentPlayersClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
